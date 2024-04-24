6dec5bd15832840ff154cf3ff1dd30d77d41ddc6
Larry Chen <102985198+LarryChenCode@users.noreply.github.com>
2024-04-23 21:57:28 -0400

    Update listGamePractice.py

--- listGamePractice.py
+++ listGamePractice.py
@@ -1,6 +1,5 @@
 import random
 
-
 print("Welcome to the List Game!!!\nNow, you'll be provided a list of words that you can play with. Are you ready?")
 input("Press Enter to continue...\n")
 
@@ -9,8 +8,7 @@ words =[
     "kiwi", "lemon", "mango", "nectarine", "orange", "pear", "quince", "raspberry", "strawberry", "tomato",
     "umbrella", "vanilla", "watermelon", "xigua", "yogurt", "zebra"
 ]
+
 random_words = random.sample(words, 10)
 print(random_words)
 
-# defaultList = "".split()
-# print("Here is the default list: ", defaultList)
